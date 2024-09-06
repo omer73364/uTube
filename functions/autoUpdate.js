db4536f637cf744d6177d8f23d17830d644f34de
omer73364 <anwar733649039@gmail.com>
2024-09-06 03:39:40 +0300

    auto update

---
+++ functions/autoUpdate.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import util from "util";
+import { exec as normal_exec } from "child_process";
+import { oraPromise } from "ora";
+const exec = util.promisify(normal_exec);
+
+const getLatestVersion = async () => {
+  const { data } = await axios.get(
+    "https://raw.githubusercontent.com/omer73364/uTube/main/version.js",
+    { responseType: "text" }
+  );
+  const srartIndex = data.indexOf('"') + 1;
+  const endIndex = data.indexOf('"', srartIndex);
+  return Number(data.slice(srartIndex, endIndex).replace(/\./g, ""));
+};
+
+export const autoUpdate = async () => {
+  const { version } = await import(`${global.packagePath}/version.js`);
+  const currentVersion = Number(version.replace(/\./g, ""));
+  const latestVersion = await getLatestVersion();
+  if (currentVersion < latestVersion) {
+    console.clear();
+    await oraPromise(exec("npm update -g utube-cli"), "- Updating uTube..");
+    logger.bold.cyan("  - [uTube]: Updated Successfully, now try again ✅\n");
+    process.exit();
+  }
+};
--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import { initialize } from "./functions/intialize.js";
 import { welcome } from "./functions/welcome.js";
 import { readArgs } from "./functions/readInput.js";
 import { download } from "./functions/download.js";
+import { autoUpdate } from "./functions/autoUpdate.js";
 
 (async () => {
   try {
     initialize();
+    await autoUpdate();
     const { url, quality } = await readArgs();
     await initialize(true);
     welcome();
